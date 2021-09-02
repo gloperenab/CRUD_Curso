2a7a551d962dab40b2f1d910f3b1eeaad0d461a9
gloperenab <43664923+gloperenab@users.noreply.github.com>
2021-09-02 16:01:36 -0500

    finalizado

---
+++ api/routes/api.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\MovieController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Models\Movie;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('movies', [MovieController::class,'index']);
+
+Route::get('movies/{id}', [MovieController::class,'show']);
+
+Route::post('movies', [MovieController::class,'store']);
+
+Route::put('movies/{id}', [MovieController::class,'update']);
+
+Route::delete('movies/{id}', [MovieController::class,'delete']);
+
